aa27c2b2bfb098c0391a2730168c176648898729
Santtu Seppänen <panttu429@gmail.com>
2016-03-24 14:40:40 +0200

    heroku conf

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -5,17 +5,20 @@ import akka.actor.ActorSystem
 import akka.actor.Props
 import spray.can.Http
 import scala.io.StdIn
+import util.Properties
 import config.Configuration
 
 object Boot extends App with Configuration{
 
 	implicit val system = ActorSystem("calculator")
-  println("config: " + serviceHost + " port " + servicePort)
-	
+    // for Heroku compatibility
+  val myPort = Properties.envOrElse("PORT", "8081").toInt 
+  println("config: " + serviceHost + " port " + myPort)
+  
   val restService = system.actorOf(Props[CalculatorServiceActor], "calculator-service")
   println("service: " + restService)
 
   // Starts HTTP server with calcuator service actor as a handler
-	IO(Http) ! Http.Bind(restService, serviceHost, servicePort)
-  }
+	IO(Http) ! Http.Bind(restService, "0.0.0.0", myPort)  
+}
 
