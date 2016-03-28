211555eadbf98bfd6e547b69e365c0a064cc9b15
Santtu Seppänen <panttu429@gmail.com>
2016-03-28 22:46:49 +0300

    fixed service host

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -13,7 +13,7 @@ object Boot extends App with Configuration{
 	implicit val system = ActorSystem("calculus")
     // for Heroku compatibility
   	val myPort = Properties.envOrElse("PORT", "8081").toInt
-  	val myHost = Properties.envOrElse("HOST", "localhost")  
+  	val myHost = Properties.envOrElse("HOST", "0.0.0.0")  
   	println("config: " + myHost + " port " + myPort)
   
   	val restService = system.actorOf(Props[CalculusServiceActor], "calculus-service")
